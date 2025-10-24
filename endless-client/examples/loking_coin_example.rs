e07788461250571dde29299481ec1f222641a8af
Alon <miki20141110@gmail.com>
2025-10-24 10:49:57 +0800

    #new: loking_coin example: test_get_stakers_unlock_info

--- endless-client/examples/loking_coin_example.rs
+++ endless-client/examples/loking_coin_example.rs
@@ -1,46 +1,16 @@
 use base_infra::result::AppResult;
 use endless_client::client::EnhancedClient;
-use endless_client::sdk_ext::account::LocalAccountExt;
 use endless_client::sdk_ext::types::ViewFnArgs;
 use endless_client::utils::account_ext::ToAccountAddress;
 use endless_client::utils::bcs_ext::BcsExt;
 use endless_sdk::move_types::account_address::AccountAddress;
-use endless_sdk::rest_client::Response;
-use endless_sdk::types::LocalAccount;
-
-fn test_recover_account() -> AppResult<LocalAccount> {
-    let account =
-        "0x4e4d1a17673091a707d786004f6ba7f86ff41396062c42b1dbffd734af9334f3".recover_account()?;
-    Ok(account)
-}
 
 base_infra::gen_impl_code_enum! {
      TestErr {
-        GetUnlockedBalance = ("TST001", "Get unlocked balance failed"),
+        GetLockingCoinErr = ("TST001", "Get locking_coin_ex failed"),
 
      }
 }
-
-// ido addr
-// 9MprBvkH5jJnK1LaHnuBMAPBQwytF1qBUBvaC8R5vYT8
-//
-// 6ye6K1mHmCje3PLb38cFPx2xdqg4zyTLYdLUdTYrQdU8
-//
-// FSHZyivVsnbeS2JJrjM4fuPjv8a2jFB6DyHSyr1hEkP4
-//
-// 9VLUbDgGiAk8vif5vzycgAHxPCNxcr5SBhMgTbuQPbvv
-//
-// yHE9ZB3yjcq7BRkb2YVJLGeLAGn2iTn2EV8X64Qivci
-//
-// DDjpRZeMcr9VAUT2FKEKUkSEqEFxSqsYAzpUG3tquKe9
-//
-// 2n2xQgjeueV77Bv63vTJFZgADhaHQrshBpcsuFwckzga
-//
-// APW7VrGceBnosH74WhsHWDLKj7JgKrFYYknNb7ob5Ky4
-//
-// Bsbpaoai7xQzXFSona9H1PmKfoQAHgBzMYAZwgghYexx
-//
-// HWcPpjxKPsTwDbsGbzMj82jV4sb5RwHocGXsQ9tcF9sA
 
 // unlocked_balance
 async fn test_unlocked_balance(client: &EnhancedClient) -> AppResult<()> {
@@ -52,7 +22,7 @@ async fn test_unlocked_balance(client: &EnhancedClient) -> AppResult<()> {
     let (mod_name, fn_name) = ("locking_coin_ex", "unlocked_balance");
     let args = ViewFnArgs::new(mod_addr, mod_name, fn_name, args, vec![])?;
     let res: u128 = client
-        .view_fn(args, &TestErr::GetUnlockedBalance, None)
+        .view_fn(args, &TestErr::GetLockingCoinErr, None)
         .await?;
     println!("unlocked_balance: {}", res);
     Ok(())
@@ -81,9 +51,48 @@ async fn test_get_unlock_info(client: &EnhancedClient) -> AppResult<()> {
     let (mod_name, fn_name) = ("locking_coin_ex", "get_unlock_info");
     let args = ViewFnArgs::new(mod_addr, mod_name, fn_name, args, vec![])?;
     let res: UnlockInfo = client
-        .view_fn(args, &TestErr::GetUnlockedBalance, None)
+        .view_fn(args, &TestErr::GetLockingCoinErr, None)
         .await?;
     println!("get_unlock_info: {:?}", res);
+    Ok(())
+}
+
+// ido addr
+// 9MprBvkH5jJnK1LaHnuBMAPBQwytF1qBUBvaC8R5vYT8
+//
+// 6ye6K1mHmCje3PLb38cFPx2xdqg4zyTLYdLUdTYrQdU8
+//
+// FSHZyivVsnbeS2JJrjM4fuPjv8a2jFB6DyHSyr1hEkP4
+//
+// 9VLUbDgGiAk8vif5vzycgAHxPCNxcr5SBhMgTbuQPbvv
+//
+// yHE9ZB3yjcq7BRkb2YVJLGeLAGn2iTn2EV8X64Qivci
+//
+// DDjpRZeMcr9VAUT2FKEKUkSEqEFxSqsYAzpUG3tquKe9
+//
+// 2n2xQgjeueV77Bv63vTJFZgADhaHQrshBpcsuFwckzga
+//
+// APW7VrGceBnosH74WhsHWDLKj7JgKrFYYknNb7ob5Ky4
+//
+// Bsbpaoai7xQzXFSona9H1PmKfoQAHgBzMYAZwgghYexx
+//
+// HWcPpjxKPsTwDbsGbzMj82jV4sb5RwHocGXsQ9tcF9sA
+
+async fn test_get_stakers_unlock_info(client: &EnhancedClient) -> AppResult<()> {
+    let eds = "ENDLESSsssssssssssssssssssssssssssssssssssss".to_account_address()?;
+    let stakers = vec![
+        "FUH7QsNDZmsFLEXrycR9pXQer2BSgGoUQ84b4RVJkWND".to_account_address()?,
+        "HWcPpjxKPsTwDbsGbzMj82jV4sb5RwHocGXsQ9tcF9sA".to_account_address()?,
+    ];
+    let args = vec![eds.to_bytes()?, stakers.to_bytes()?];
+
+    let mod_addr = "0x1".to_account_address()?;
+    let (mod_name, fn_name) = ("locking_coin_ex", "get_stakers_unlock_info");
+    let args = ViewFnArgs::new(mod_addr, mod_name, fn_name, args, vec![])?;
+    let res: Vec<UnlockInfo> = client
+        .view_fn(args, &TestErr::GetLockingCoinErr, None)
+        .await?;
+    println!("get_stakers_unlock_info: {:?}", res);
     Ok(())
 }
 
@@ -97,7 +106,7 @@ async fn test_staking_amount(client: &EnhancedClient) -> AppResult<()> {
     let (mod_name, fn_name) = ("locking_coin_ex", "staking_amount");
     let args = ViewFnArgs::new(mod_addr, mod_name, fn_name, args, vec![])?;
     let res: u128 = client
-        .view_fn(args, &TestErr::GetUnlockedBalance, None)
+        .view_fn(args, &TestErr::GetLockingCoinErr, None)
         .await?;
     println!("staking_amount: {}", res);
     Ok(())
@@ -105,9 +114,6 @@ async fn test_staking_amount(client: &EnhancedClient) -> AppResult<()> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // recover account
-    // let mut account = test_recover_account()?;
-
     let client = EnhancedClient::new_with_url_str("https://rpc-test.endless.link/v1")?;
 
     // unlocked_balance
@@ -120,5 +126,11 @@ async fn main() -> anyhow::Result<()> {
 
     // get_unlock_info
     test_get_unlock_info(&client).await?;
+    println!("---------------------------------------");
+
+    // get_stakers_unlock_info
+    test_get_stakers_unlock_info(&client).await?;
+    println!("---------------------------------------");
+
     Ok(())
 }
