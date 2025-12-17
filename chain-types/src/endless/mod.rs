f9bc85ab30367b16a0e0bcd89432fc1c27198c7e
Alon <miki20141110@gmail.com>
2025-12-17 10:26:19 +0800

    #new: endless address 2

--- chain-types/src/endless/mod.rs
+++ chain-types/src/endless/mod.rs
@@ -1,1 +1,2 @@
+pub use move_core_types::account_address::*;
 pub mod eds_addr_ext;
--- chain-types/src/lib.rs
+++ chain-types/src/lib.rs
@@ -1,5 +1,3 @@
 #[cfg(feature = "endless")]
-pub use move_core_types::account_address::*;
-#[cfg(feature = "endless")]
 pub mod endless;
 pub mod error;
